bb35de27b53b4dcc49bb8bbd42239b19fdd45939
rishabhdevKaushik <rishabhdevkaushik87@gmail.com>
2025-05-02 16:49:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div>
+            <h1>My React App</h1>
+            <h2>my react implimentation with CI/CD</h2>
+            <h3>with github actions</h3>
+        </div>
+    );
+}
+
+export default App;
